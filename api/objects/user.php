e513e8d897f1bbbaa2a1076bb5b7ba4d6c158228
antar <toobrokeforrysll@gmail.com>
2021-06-15 14:07:40 +0200

    feat: update user credentials and generate new jwt token

--- api/objects/user.php
+++ api/objects/user.php
@@ -55,7 +55,7 @@ class User {
 		 
 	 }
 	 
-	 function emailExists(){
+	 function emailExists() {
 	  
 		 // query to check if email exists
 		 $query = "SELECT id, firstname, lastname, password
@@ -79,7 +79,7 @@ class User {
 		 $num = $stmt->rowCount();
 	  
 		 // if email exists, assign values to object properties for easy access and use for php sessions
-		 if($num>0){
+		 if ($num>0) {
 	  
 			 // get record details / values
 			 $row = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -95,6 +95,52 @@ class User {
 		 }
 	  
 		 // return false if email does not exist in the database
+		 return false;
+		 
+	 }
+	 
+	 public function update() {
+	  
+		 // if password needs to be updated
+		 $password_set=!empty($this->password) ? ", password = :password" : "";
+	  
+		 // if no posted password, do not update the password
+		 $query = "UPDATE " . $this->table_name . "
+				 SET
+					 firstname = :firstname,
+					 lastname = :lastname,
+					 email = :email
+					 {$password_set}
+				 WHERE id = :id";
+	  
+		 // prepare the query
+		 $stmt = $this->conn->prepare($query);
+	  
+		 // sanitize
+		 $this->firstname = htmlspecialchars(strip_tags($this->firstname));
+		 $this->lastname = htmlspecialchars(strip_tags($this->lastname));
+		 $this->email = htmlspecialchars(strip_tags($this->email));
+	  
+		 // bind the values from the form
+		 $stmt->bindParam(':firstname', $this->firstname);
+		 $stmt->bindParam(':lastname', $this->lastname);
+		 $stmt->bindParam(':email', $this->email);
+	  
+		 // hash the password before saving to database
+		 if (!empty($this->password)) {
+			 $this->password=htmlspecialchars(strip_tags($this->password));
+			 $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+			 $stmt->bindParam(':password', $password_hash);
+		 }
+	  
+		 // unique ID of record to be edited
+		 $stmt->bindParam(':id', $this->id);
+	  
+		 // execute the query
+		 if ($stmt->execute()) {
+			 return true;
+		 }
+	  
 		 return false;
 		 
 	 }
