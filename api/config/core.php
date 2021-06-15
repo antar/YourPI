7d2bad22e7a16f69b8a39c2d1e75fc44be4a4050
antar <toobrokeforrysll@gmail.com>
2021-06-15 13:35:32 +0200

    feat: core.php

--- api/config/core.php
+++ api/config/core.php
@@ -2,6 +2,9 @@
 // show error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//set default time-zone
+date_default_timezone_set('Europe/Zurich');
   
 // home page url
 $home_url="https://tracker.wes.fm/REST-API-PHP/api/";
@@ -14,3 +17,9 @@ $records_per_page = 5;
   
 // calculate for the query LIMIT clause
 $from_record_num = ($records_per_page * $page) - $records_per_page;
+
+// variables used for jwt
+$key = "example_key";
+$issued_at = time();
+$expiration_time = $issued_at + (60 * 60); // valid for 1 hour
+$issuer = "https://tracker.wes.fm/REST-API-PHP/api/";
