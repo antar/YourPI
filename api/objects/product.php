c40b39d87516ec392cbf754099a83644a7ab2156
antar <toobrokeforrysll@gmail.com>
2021-06-15 11:33:46 +0200

    feat: pagination for api

--- api/objects/product.php
+++ api/objects/product.php
@@ -110,7 +110,7 @@ class Product {
 		
 	}
 	
-	function update(){
+	function update() {
 	  
 		// update query
 		$query = "UPDATE
@@ -141,11 +141,109 @@ class Product {
 		$stmt->bindParam(':id', $this->id);
 	  
 		// execute the query
-		if($stmt->execute()){
+		if ($stmt->execute()) {
 			return true;
 		}
 	  
 		return false;
+		
+	}
+	
+	function delete() {
+	  
+		// delete query
+		$query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+	  
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+	  
+		// sanitize
+		$this->id=htmlspecialchars(strip_tags($this->id));
+	  
+		// bind id of record to delete
+		$stmt->bindParam(1, $this->id);
+	  
+		// execute query
+		if ($stmt->execute()) {
+			return true;
+		}
+	  
+		return false;
+		
+	}
+	
+	function search($keywords) {
+	  
+		// select all query
+		$query = "SELECT
+					c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+				FROM
+					" . $this->table_name . " p
+					LEFT JOIN
+						categories c
+							ON p.category_id = c.id
+				WHERE
+					p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ?
+				ORDER BY
+					p.created DESC";
+	  
+		// prepare query statement
+		$stmt = $this->conn->prepare($query);
+	  
+		// sanitize
+		$keywords=htmlspecialchars(strip_tags($keywords));
+		$keywords = "%{$keywords}%";
+	  
+		// bind
+		$stmt->bindParam(1, $keywords);
+		$stmt->bindParam(2, $keywords);
+		$stmt->bindParam(3, $keywords);
+	  
+		// execute query
+		$stmt->execute();
+	  
+		return $stmt;
+		
+	}
+	
+	function readPaging($from_record_num, $records_per_page) {
+	  
+		// select query
+		$query = "SELECT
+					c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+				FROM
+					" . $this->table_name . " p
+					LEFT JOIN
+						categories c
+							ON p.category_id = c.id
+				ORDER BY p.created DESC
+				LIMIT ?, ?";
+	  
+		// prepare query statement
+		$stmt = $this->conn->prepare( $query );
+	  
+		// bind variable values
+		$stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+		$stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+	  
+		// execute query
+		$stmt->execute();
+	  
+		// return values from database
+		return $stmt;
+		
+	}
+	
+	public function count() {
+		
+		//select query
+		$query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+	  
+		$stmt = $this->conn->prepare( $query );
+		$stmt->execute();
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	  
+		return $row['total_rows'];
+		
 	}
 }
-?>
