2cff3ac5847a97eea1f94dd7ae48a8bf56f0e298
antar <toobrokeforrysll@gmail.com>
2021-06-15 10:04:01 +0200

    fix: read.php

--- api/product/read.php
+++ api/product/read.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once '../config/database.php';
-include_once '../objects/product.php';
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/database.php';
+include_once '../objects/product.php';
 
 $database = new Database();
 $db = $database->getConnection();
