62853da15cd82212b7f0f63b2ec56018651a30ef
Lars Peder <lars.peder@08eins.swiss>
2021-06-16 08:27:30 +0200

    Router

--- yourpi/src/App.tsx
+++ yourpi/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/login" exact>
+          <h1>Login</h1>
+        </Route>
+        <Route path="/sign-up" exact>
+          <h1>Sign Up</h1>
+        </Route>
+        <Route path="/" exact>
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- yourpi/src/components/Header.tsx
+++ yourpi/src/components/Header.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">YourPI</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>YourPI</Navbar.Brand>
+        </LinkContainer>
         <Nav className="me-auto">
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/sign-up">
+            <Nav.Link>Sign Up</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Container>
     </Navbar>
